d6bb88ea604da1a138c86d1a5ec3449371f02829
Duan-JM <vincent.duan95@gmail.com>
2026-05-15 16:53:00 +0800

    remove dirty code

--- t2i_example.sh
+++
@@ -1,7 +0,0 @@
-curl -X POST http://10.170.138.159:8891/v1/images/generations \
-  -H "Content-Type: application/json" \
-  -d '{
-    "prompt": "a dragon laying over the spine of the Green Mountains of Vermont",
-    "size": "1024x1024",
-    "seed": 42
-  }' | jq -r '.data[0].b64_json' | base64 -d > dragon.png
